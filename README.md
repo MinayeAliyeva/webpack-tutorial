76f8ef5a37e23bf4aaac8a0bd40b3d2ca2ed1f76
MinayeAliyeva <7s346pu@code.edu.az>
2024-11-14 17:13:47 +0400

    webpack practise added

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 //component=>button ->klik edende div component olacaq bu div olacaq
+Webpack müasir JavaScript proqramları üçün statik modul paketidir.
--- index.js
+++ index.js
@@ -4,3 +4,17 @@ console.log("mmm");
 
 const imgComponent = new Image();
 imgComponent.render();
+const buttonComponent = document.createElement("button");
+buttonComponent.textContent = "Click";
+document.body.appendChild(buttonComponent);
+let count = 0;
+buttonComponent.addEventListener("click", () => {
+  count++;
+  console.log("click");
+  const createdDiv = document.createElement("div");
+  createdDiv.textContent = `${count}div`;
+  createdDiv.style.border = "1px solid black";
+  createdDiv.style.margin = "5px";
+  createdDiv.style.padding = "10px";
+  document.body.appendChild(createdDiv)
+});
